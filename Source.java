fe6b4352e231f81f401425fa54f559d5f1945b88
Duncan McLean <duncan.mclean@fiserv.com>
2023-11-06 14:51:55 -0500

    AH9 Complete

--- Source.java
+++ Source.java
@@ -1,33 +1,47 @@
+import java.util.Stack;
+
 class Node<T> {
-    T val;
-    Node<T> next;
-    
-    public Node(T val) {
-      this.val = val;
-      this.next = null;
+  T val;
+  Node<T> next;
+
+  public Node(T val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Source {
+  public static <T> Node<T> reverseList(Node<T> head) {
+    Node<T> prev = null;
+    Node<T> current = head;
+    Node<T> temp;
+
+    while (current != null) {
+      temp = current.next;
+      current.next = prev;
+      prev = current;
+      current = temp;
+    }
+
+    return prev;
+  }
+
+  public static void main(String[] args) {
+    Node<String> x = new Node<>("x");
+    Node<String> y = new Node<>("y");
+    Node<String> z = new Node<>("z");
+
+    x.next = y; // x -> y
+    y.next = z;
+
+    reverseList(x); // y -> x
+
+    // Printing solution
+    Node<String> head = Source.reverseList(x);
+
+    while (head != null) {
+      System.out.print(head.val + " ");
+      head = head.next;
     }
   }
-  
-  class Source {
-    public static <T> Node<T> reverseList(Node<T> head) {
-      // todo
-      return head;
-    }
-    
-    public static void main(String[] args) {
-        Node<String> x = new Node<>("x");
-        Node<String> y = new Node<>("y");
-    
-        x.next = y; // x -> y
-    
-        reverseList(x); // y -> x
-
-        // Printing solution
-        Node<String> head = Source.reverseList(x);
-
-        while (head != null) {
-            System.out.println(head.val);
-            head = head.next;            
-        }
-    }
-  }
+}
